5303b8ac0abbf7da1b32e455379c827875e45193
osawa.masashi <osawa.masashi@gmail.com>
2026-05-22 08:52:06 +0900

    fix: use Pos type instead of any in vimCustomMappings

--- web/src/lib/vimCustomMappings.ts
+++ web/src/lib/vimCustomMappings.ts
@@ -1,15 +1,14 @@
-import { Vim } from "@replit/codemirror-vim";
+import { type Pos, Vim } from "@replit/codemirror-vim";
 
 // g_ is not in @replit/codemirror-vim's default key map, so we add it here.
 // It moves the cursor to the last non-blank character of the current line.
-Vim.defineMotion("moveToLastNonBlankChar", (cm, head) => {
+Vim.defineMotion("moveToLastNonBlankChar", (cm, head): Pos => {
   const lineText: string = cm.getLine(head.line);
   let col = lineText.length - 1;
   while (col > 0 && (lineText[col] === " " || lineText[col] === "\t")) {
     col--;
   }
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return { line: head.line, ch: Math.max(0, col) } as any;
+  return { line: head.line, ch: Math.max(0, col) };
 });
 
 Vim.mapCommand("g_", "motion", "moveToLastNonBlankChar", {}, {});
